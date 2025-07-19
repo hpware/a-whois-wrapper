61553322f6fa2d20d7312b40f0e85018cef413a1
吳元皓 <hw@yuanhau.com>
2025-07-19 22:25:25 +0800

    Add deploying stage (And GitHub CI/CD).

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import react from '@astrojs/react';
-
-import vue from '@astrojs/vue';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import vue from "@astrojs/vue";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), vue()]
+  integrations: [react(), vue()],
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
